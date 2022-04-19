dbb2ad8687f8a50b826ce329df5bbe888468b0c3
Andrea <astamariab@gmail.com>
2022-04-19 20:09:22 +0200

    php tweet

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
-$tweet = "En un lugar de la Mancha...";
-$author = "Cervantes";
-$user = "soycervantes";
-
-
+// $tweet = "En un lugar de la Mancha...";
+// $author = "Cervantes";
+// $user = "soycervantes";
+$data= file_get_contents("tweets.json");
+$jsonData= json_decode($data, true);
 
 require "index.html";
