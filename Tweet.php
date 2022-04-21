9cc2fedadf90cc59bf32c0a8891c0d78b1200fe9
Rafael Basterra Careaga <rbasterra@msn.com>
2022-04-21 19:36:57 +0200

    adds renderTweet method

--- Tweet.php
+++ Tweet.php
@@ -139,4 +139,40 @@ class Tweet
 
     }
 
+    public function renderTweet() {
+        return '<div class="post">
+        <div class="post__avatar">
+          <img
+            src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
+            alt=""
+          />
+        </div>
+
+        <div class="post__body">
+          <div class="post__header">
+            <div class="post__headerText">
+              <h3>'.
+                $this -> user .'
+                <span class="post__headerSpecial"
+                  ><span class="material-icons post__badge"> verified </span>@'. $this -> userName .'</span
+                >
+              </h3>
+            </div>
+            <div class="post__headerDescription">
+              <p>'. $this -> text .'</p>
+            </div>
+          </div>
+          <img
+            src="https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03.jpg"
+            alt=""
+          />
+          <div class="post__footer">
+            <span class="material-icons"> repeat </span>
+            <span class="material-icons"> favorite_border </span>
+            <span class="material-icons"> publish </span>
+          </div>
+        </div>
+      </div>';
+    }
+
 }
